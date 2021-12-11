51d2690e842b43197215a7af869b37adefe80591
VV <85766798+Vedant-cll@users.noreply.github.com>
2021-12-11 15:11:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(590,590);
+canvas.center();   
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('vedant.png');
+}
